15209452d392d1d707d816bc3e736555d37af9cc
Richard Lewin <>
2019-07-17 10:14:28 +0100

    Removed files to enable plugin to work correctly alongside using "LaunchStoryboard" method of showing a splashscreen. This code will NOT work alongside using the "LaunchImage" method of showing a splashscreen. Bumped version number.

--- src/ios/SplitScreen.h
+++
@@ -1,7 +0,0 @@
-#import <Cordova/CDV.h>
-
-@interface SplitScreen : CDVPlugin
-
-- (void) dummy:(CDVInvokedUrlCommand*)command;
-
-@end
--- www/splitscreen.js
+++
@@ -1,7 +0,0 @@
-/*global cordova, module*/
-
-module.exports = {
-	dummy: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "SplitScreen", "dummy", []);
-    }
-};
